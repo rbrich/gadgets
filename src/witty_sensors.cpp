694706d098050beda997ff0083ef04988d5dc57a
Radek Brich <radek.brich@devl.cz>
2019-02-21 08:48:11 +0100

    New device: Wemos D1 mini Pro v1.1.0

--- src/witty_sensors.cpp
+++ src/witty_sensors.cpp
@@ -8,34 +8,44 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-// Note that the pin numbers on PCB are the real numbers, eg. GPIO4 is 4, not D4
-static const int pin_led = 2;
-static const int pin_ldr = A0;
+#ifdef WITH_RGB
 static const int pin_rgb_red = 15;
 static const int pin_rgb_green = 12;
 static const int pin_rgb_blue = 13;
+#endif
 
-int timer = 0;
+#ifdef WITH_LDR
+static const int pin_ldr = A0;
+#endif
 
-OneWire temp_wire(4);
-DallasTemperature temp_sensor(&temp_wire);
-DeviceAddress temp_addr;
+#ifdef WITH_DALLAS_TEMP
+static OneWire temp_wire(4);
+static DallasTemperature temp_sensor(&temp_wire);
+static DeviceAddress temp_addr;
+#endif
 
+static int timer = 0;
 
 void setup()
 {
     // Connect with: pio device monitor
-    Serial.begin(9600);
+    Serial.begin(115200);
     Serial.println();
     Serial.println("=== Setup ===");
 
-    // LED + LDR pins
-    pinMode(pin_led, OUTPUT);
-    pinMode(pin_ldr, INPUT);
+    // LED pins
+    pinMode(LED_BUILTIN, OUTPUT);
+#ifdef WITH_RGB
     pinMode(pin_rgb_red, OUTPUT);
     pinMode(pin_rgb_green, OUTPUT);
     pinMode(pin_rgb_blue, OUTPUT);
+#endif
 
+#ifdef WITH_LDR
+    pinMode(pin_ldr, INPUT);
+#endif
+
+#ifdef WITH_DALLAS_TEMP
     // temperature sensor
     Serial.println("--- Temperature ---");
     temp_sensor.begin();
@@ -64,6 +74,7 @@ void setup()
     // sensors.setResolution(insideThermometer, 12);
     Serial.print(temp_sensor.getResolution(temp_addr), DEC);
     Serial.println();
+#endif
 
     // Setup Wi-Fi
     Serial.println("--- Wi-Fi ---");
@@ -89,10 +100,12 @@ void loop()
     timer++;
 
     // Reset LEDs
-    digitalWrite(pin_led, HIGH);
+    digitalWrite(LED_BUILTIN, HIGH);
+#ifdef WITH_RGB
     analogWrite(pin_rgb_red, 0);
     analogWrite(pin_rgb_green, 0);
     analogWrite(pin_rgb_blue, 0);
+#endif
     delay(500);
 
     // Present remaining time using RGB diode
@@ -106,6 +119,7 @@ void loop()
         Serial.println(t_mins);
     } else {
         Serial.print(".");
+#ifdef WITH_RGB
         int value = t_secs;
         if (t_mins == 1 || t_mins == 2 || t_mins == 4)
             analogWrite(pin_rgb_red, value);
@@ -113,6 +127,7 @@ void loop()
             analogWrite(pin_rgb_green, value);
         if (t_mins == 0 || t_mins == 2)
             analogWrite(pin_rgb_blue, value);
+#endif
     }
     delay(500);
 
@@ -120,7 +135,7 @@ void loop()
     if (t_mins == 5 && t_secs == 0) {
         // Trigger the action
         timer = 0;
-        digitalWrite(pin_led, LOW);
+        digitalWrite(LED_BUILTIN, LOW);
     } else {
         // Not yet
         return;
@@ -128,17 +143,21 @@ void loop()
 
     // ------------------------------------------------------------------------
 
+#ifdef WITH_LDR
     // Read light sensor
     int ldr_value = analogRead(pin_ldr);
     Serial.print("LDR: ");
     Serial.println(ldr_value);
+#endif
 
+#ifdef WITH_DALLAS_TEMP
     // Read temperature sensor
     temp_sensor.requestTemperaturesByAddress(temp_addr);
     float temp_celsius = temp_sensor.getTempC(temp_addr);
     Serial.print("Temperature: ");
     Serial.print(temp_celsius);
     Serial.println("°C");
+#endif
 
     // Send values to InfluxDB:
     WiFiClient client;
@@ -147,14 +166,23 @@ void loop()
     if (client.connect(DB_HOST, DB_PORT)) {
         Serial.printf("* Connected (%s)\n", client.remoteIP().toString().c_str());
         Serial.println("* Sending data...");
-        String data("ambient_light," DEVICE_TAGS " value=");
+
+        String data;
+
+#ifdef WITH_LDR
+        data.concat("ambient_light," DEVICE_TAGS " value=");
         data.concat(ldr_value);
         data.concat('\n');
+#endif
+
+#ifdef WITH_DALLAS_TEMP
         if (temp_celsius != DEVICE_DISCONNECTED_C) {
             data.concat("temperature," DEVICE_TAGS " value=");
             data.concat(temp_celsius);
             data.concat('\n');
         }
+#endif
+
         client.printf(
                 "POST /write?db=" DB_NAME " HTTP/1.1\r\n"
                 "Host: " DB_HOST ":%d\r\n"
